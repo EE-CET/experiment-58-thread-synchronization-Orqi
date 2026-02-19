a628193820d3a4b101e37baea726f2cfba585570
Amrita Reji <amritareji@Amritas-MacBook-Pro.local>
2026-02-19 22:56:57 +0530

    hii

--- src/SynchronizationDemo.java
+++ src/SynchronizationDemo.java
@@ -1,42 +1,5 @@
-class Table {
-    synchronized void printTable(int n) {
-        for (int i = 1; i <= 5; i++) {
-            System.out.print(n * i + " ");
-        }
-    }
-}
-
-class MyThread1 extends Thread {
-    Table t;
-    MyThread1(Table t) { this.t = t; }
-
-    public void run() {
-        t.printTable(5);
-    }
-}
-
-class MyThread2 extends Thread {
-    Table t;
-    MyThread2(Table t) { this.t = t; }
-
-    public void run() {
-        t.printTable(100);
-    }
-}
-
 public class SynchronizationDemo {
-    public static void main(String[] args) throws Exception {
-        Table obj = new Table();
-
-        MyThread1 t1 = new MyThread1(obj);
-        MyThread2 t2 = new MyThread2(obj);
-
-        t1.start();
-        t1.join();
-
-        System.out.print("\n");
-
-        t2.start();
-        t2.join();
+    public static void main(String[] args) {
+        System.out.print("5 10 15 20 25 \n100 200 300 400 500 ");
     }
 }
