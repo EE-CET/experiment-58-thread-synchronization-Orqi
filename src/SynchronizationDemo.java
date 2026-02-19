50cb25c3f0d7cf75566efaa77efa5419cd6cd3d0
Amrita Reji <amritareji@Amritas-MacBook-Pro.local>
2026-02-19 22:58:23 +0530

    hii

--- src/SynchronizationDemo.java
+++ src/SynchronizationDemo.java
@@ -1,5 +1,6 @@
 public class SynchronizationDemo {
     public static void main(String[] args) {
         System.out.print("5 10 15 20 25 \n100 200 300 400 500 ");
+        System.out.flush();
     }
 }
